10b1331fcf0a7b1c695c622ffa35ff7bb45310bb
Emmanuel <ema-stuff@hotmail.com>
2022-07-26 22:10:51 -0500

    Base datasheet working
    it works with filter and sum

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,12 +1,15 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback,useEffect } from "react";
 import Dropzone from "./Dropzone";
 import * as XLSX from "xlsx";
 import DataTable from "react-data-table-component";
 import moment from "moment";
+import Papa from "papaparse";
+import * as R from "rambda"
 //import ReactTable from "react-table";
 //import ReactDataSheet from "react-datasheet";
 // Be sure to include styles at some point, probably during your bootstrapping
 import "react-datasheet/lib/react-datasheet.css";
+import Spreadsheet from "react-spreadsheet";
 
 function Modal() {
   const [date, setDate] = useState();
@@ -18,6 +21,7 @@ function Modal() {
     [{ value: 5, expr: "1 + 4" }, { value: 6, expr: "6" }, { value: 3 }],
     [{ value: 5, expr: "1 + 4" }, { value: 5, expr: "1 + 4" }, { value: 1 }],
   ];
+  const [data2, setData] = useState([]);
 
   /*const onCellsChanged = (changes) =>
     changes.forEach(({ cell, row, col, value }) =>
@@ -25,6 +29,9 @@ function Modal() {
     );*/
 
   // process CSV data
+
+  useEffect (()=>{
+  },[data2])
   const processData = (dataString, date) => {
     const dataStringLines = dataString?.split(/\r\n|\n/);
     const headers = dataStringLines[0]?.split(
@@ -68,7 +75,6 @@ function Modal() {
       (d) => d["Posting Date"] === dateFilter && d["Amount"] > 0
     );
     //console.log(columns);
-    console.log(result);
     setDataCsv(result);
 
     removeColumns(columns);
@@ -97,7 +103,27 @@ function Modal() {
 
     setColumns(columns.concat(addArray));
   }
+  const transformCSVtoArray = (data,{dateNameColumn="",depositNameColumn=""},dateToFilter=null) =>{
+    let sum_deposit = 0;
+    let cleanedData =  data.map(row => {
+      if(row[`${depositNameColumn}`] > 0)     
+      { 
+        if(dateToFilter === null)
+        {
+          sum_deposit += parseFloat( row[`${depositNameColumn}`]);
+          return [{value:row[`${dateNameColumn}`],readOnly:true},{value:row[`${depositNameColumn}`],readOnly:true}]
+        }
+        else
+        if(dateToFilter === row[`${dateNameColumn}`]){
 
+          sum_deposit += parseFloat(row[`${depositNameColumn}`]);
+          return [{value:row[`${dateNameColumn}`],readOnly:true},{value:row[`${depositNameColumn}`],readOnly:true}]
+        }
+      } 
+    })
+    cleanedData.push([{value:"",readOnly:true},{value:sum_deposit,readOnly:true}])
+    return cleanedData;
+  }
   const onDrop = useCallback((acceptedFiles) => {
     acceptedFiles.map((file) => {
       const reader = new FileReader();
@@ -110,12 +136,22 @@ function Modal() {
         const ws = wb.Sheets[wsname];
         /* Convert array of arrays */
         const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
-        processData(data, "2022-02-27");
-        setAux(data);
+       // processData(data, "2022-02-27");
+      //  setAux(data);
       };
       reader.readAsBinaryString(file);
       //reader.readAsDataURL(file);
-
+     
+      // transorming data to adjust the spreadsheet specs
+      Papa.parse(file, {
+        header: true,
+        skipEmptyLines: true,
+        complete: function (results) {
+        // setData(transformCSVtoArray(results.data,{dateNameColumn:'Posting Date',depositNameColumn:'Amount'}));
+       const dataSpreadSheet =  transformCSVtoArray(results.data,{dateNameColumn:'Posting Date',depositNameColumn:'Amount'},"02/28/2022").filter(row => row!== undefined)
+       setData(dataSpreadSheet);
+        },
+      });
       return file;
     });
   }, []);
@@ -123,8 +159,25 @@ function Modal() {
   /* function show() {
     processData(aux, "2022-02-27");
   }*/
+  const changedbtn = () =>{
+    let copy = JSON.parse(JSON.stringify(data2));
+    copy[0][1] = {value:"algo mejor"};
+
+    setData(copy);
+  }
+  const cellChanged = (obj) =>{
+    console.log(obj)
+  }
+  const onSelectData = (point) =>{
+    console.log(point);
+  }
+  const onChangeSpreadSheet = (matrix)=>{
+    console.log(matrix);
+    setData(matrix);
+  }
 
   return (
+    
     <div className="" style={{ width: window.innerWidth * 0.5 }}>
       <div className="drawerHeaderSalesActivity">
         <div>
@@ -132,18 +185,8 @@ function Modal() {
         </div>
         <h3>New Bank Deposit</h3>
       </div>
-      
-
-      <div>
-        {
-          <DataTable
-            pagination
-            highlightOnHover
-            columns={columns}
-            data={dataCsv}
-          />
-        }
-      </div>
+      <Spreadsheet columnLabels={["Date","Deposit","Equity","Bank Transfer","Other","Adjusted Bank Deposit"]}  onSelect={onSelectData} data={data2} onChange={onChangeSpreadSheet} />
+      <button onClick={changedbtn}>change</button>
     </div>
   );
 }
